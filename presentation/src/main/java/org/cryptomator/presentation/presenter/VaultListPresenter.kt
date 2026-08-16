741127aa317e759b5e1c19a1e65daff1fcf540f5
Misab <misab@protonmail.com>
2026-08-16 21:25:41 +0530

    Remove trial expiration logic from resumed method
    Removed trial expiration check for freemium flavor.

--- presentation/src/main/java/org/cryptomator/presentation/presenter/VaultListPresenter.kt
+++ presentation/src/main/java/org/cryptomator/presentation/presenter/VaultListPresenter.kt
@@ -106,14 +106,6 @@ class VaultListPresenter @Inject constructor( //
 		if (launchWelcomeFlowIfNeeded()) {
 			return
 		}
-
-		if (FlavorConfig.isFreemiumFlavor) {
-			val alreadyKnownExpired = sharedPreferencesHandler.isTrialExpired()
-			val trialState = licenseEnforcer.evaluateTrialState()
-			if (!alreadyKnownExpired && trialState.isExpired && !licenseEnforcer.hasPaidLicense()) {
-				view?.showDialog(TrialExpiredDialog.newInstance())
-			}
-		}
 	}
 
 	private fun launchWelcomeFlowIfNeeded(): Boolean {
